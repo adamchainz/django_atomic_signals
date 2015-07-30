029cb5ef2e111b7b331e3b334d02c692795211f9
Adam Chainz <adam@adamj.eu>
2015-07-30 19:21:29 +0100

    Test with MySQL because that's what I know

--- tests/settings.py
+++ tests/settings.py
@@ -1,25 +1,33 @@
 import os
 
 ADMINS = ()
-DATABASES = {}
 
 
 database_implementation = os.getenv('DATABASE', 'sqlite3')
 
-DATABASES['default'] = {
-    'sqlite3': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'django_atomic_signals.db',
-    },
-    'postgresql': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': os.getenv('DATABASE_USER', 'postgres'),
-        'NAME': 'djts',
-        'OPTIONS': {
-            'autocommit': True,
-        },
-    },
-}[database_implementation]
+if database_implementation == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'django_atomic_signals.db',
+        }
+    }
+elif database_implementation == 'mysql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'django_mysql',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+            'OPTIONS': {'charset': 'utf8mb4'},
+            'TEST': {
+                'COLLATION': "utf8mb4_general_ci",
+                'CHARSET': "utf8mb4"
+            }
+        }
+    }
 
 SECRET_KEY = '_uobce43e5osp8xgzle*yag2_16%y$sf*5(12vfg25hpnxik_*'
 
